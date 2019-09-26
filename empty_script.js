bf6b7cbe678175363eee929321afbd28557be4c3
zzebrum <44832195+zzebrum@users.noreply.github.com>
2019-09-26 14:48:55 +0300

    Update empty_script.js

--- empty_script.js
+++ empty_script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         script doing nothing
-// @version      0.1
+// @version      0.2
 // @description  what a shame
 // @match        *://*/*
 // @grant        unsafeWindow
